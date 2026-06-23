6ef924837c7473b79025010874759ff6bfcf8a4c
xkendovul <hadyihernandez@gmail.com>
2026-06-23 02:10:48 -0600

    cambio variables de entorno

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
       transport: Transport.TCP,
       options: {
         host: '0.0.0.0',
-        port: Number(process.env.PORT) || 3002,
+        port: Number(process.env.PORT),
       },
     },
   );
