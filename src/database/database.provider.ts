62b64a1a4a47bbc76b6982a3eea259a4f9d0b57a
xkendovul <hadyihernandez@gmail.com>
2026-06-22 21:38:50 -0600

    cambio variables de entorno

--- src/database/database.provider.ts
+++ src/database/database.provider.ts
@@ -1,19 +1,21 @@
 import { DynamicModule } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 export const DatabaseProvider: DynamicModule = TypeOrmModule.forRootAsync({
   imports: [ConfigModule],
   inject: [ConfigService],
-
+  // Use useFactory, useClass, or useExisting
+  // to configure the DataSourceOptions.
   useFactory: (configService: ConfigService) => ({
     type: 'postgres',
     host: configService.get('HOST'),
-    port: +configService.get('PORT'),
+    port: +configService.get('PORT_DB'),
     username: configService.get('USERNAME_DB'),
     password: configService.get('PASSWORD_DB'),
     database: configService.get('DATABASE'),
+    entities: [],
     autoLoadEntities: true,
-    synchronize: process.env.NODE_ENV !== 'production',
+    synchronize: false,
   }),
 });
